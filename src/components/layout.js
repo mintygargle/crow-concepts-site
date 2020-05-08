b5002b3753362661504b37a57a7a81447c6ea7bc
Michael Tanner <michaelmontytanner@gmail.com>
2020-05-07 23:46:07 -0500

    Component test

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
+import { Header, Footer } from "./Nav/nav_index"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,7 +26,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Header></Header>
       <div
         style={{
           margin: `0 auto`,
@@ -39,6 +41,8 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
+
+        <Footer></Footer>
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,7 @@ import PostFetcher from "../components/postFetcher"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <PostFetcher></PostFetcher>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Home Page</h1>
   </Layout>
 )
 

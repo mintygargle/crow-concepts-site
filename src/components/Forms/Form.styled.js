b0a6f53b7948e21078e45f9d6c801d9f1fc7791b
Michael <michaelmontytanner@gmail.com>
2020-06-03 00:50:05 -0500

    Border code for forms

--- src/components/Forms/Form.styled.js
+++ src/components/Forms/Form.styled.js
@@ -1,9 +1,13 @@
-import styled from "styled-components";
-import { default as component } from "./Form";
+import styled from "styled-components"
+import { default as component } from "./Form"
 
 const Form = styled(component)`
   width: 50%;
   padding: 1em;
-`;
+  border-style: solid;
+  border-radius: 16px;
+  border-width: 1px;
+  border-color: "#f8f8f8";
+`
 
-export default Form;
+export default Form

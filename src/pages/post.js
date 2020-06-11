44fb130241955be2f00e9ad6cc5b7a1edbf0eeff
michael-tanner-coder <michaelmontytanner@gmail.com>
2020-06-11 13:26:50 -0500

    Switch to using page history instead of page location for blog posts

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,21 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import * as Text from "../components/Text/text_index"
+import { useEffect } from "react"
 
 const PostPage = ({ location, history }) => {
-  const state = location.state || (typeof history === "object" && history.state)
+  // const state = location.state || (typeof history === "object" && history.state)
+  const [state, setState] = useState(null)
 
-  return (
-    <Layout>
-      <br />
-      <br />
-      <article>
-        <Text.H1>{state.post.title}</Text.H1>
-        <Text.H2>{state.post.author}</Text.H2>
-        <Text.P>{state.post.body}</Text.P>
-      </article>
-    </Layout>
-  )
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return
+    }
+
+    setState({ post: location.state.post })
+  })
+
+  if (state) {
+    return (
+      <Layout>
+        <br />
+        <br />
+        <article>
+          <Text.H1>{state.post.title}</Text.H1>
+          <Text.H2>{state.post.author}</Text.H2>
+          <Text.P>{state.post.body}</Text.P>
+        </article>
+      </Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <br />
+        <br />
+        <h1>Loading</h1>
+      </Layout>
+    )
+  }
 }
 
 export default PostPage
